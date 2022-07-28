758c9a3fe1ee17877fcc672758561c49e60609c2
João Gustavo <joaogustavosilva2001@gmail.com>
2022-07-28 19:00:06 -0300

    Create styled formDataContainer

--- components/DataSubmissionForm/styles.ts
+++ components/DataSubmissionForm/styles.ts
@@ -1,3 +1,17 @@
 import styled from "styled-components";
 
-export const FormDataContainer = styled.div``
+export const FormDataContainer = styled.form`
+padding: 0.5rem;
+margin: 0.5rem;
+border-radius: 0.25rem;
+max-height: 100%;
+background-color: ${props => props.theme['gray-50']};
+`
+
+export const FormGroupContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+
+    width: 37.5rem; // 600px
+    margin: 0 auto;
+`
