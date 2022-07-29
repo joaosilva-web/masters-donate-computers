2cb0c3eb9dc3c753ca8a7dc769d6114150b9b92e
João Gustavo <joaogustavosilva2001@gmail.com>
2022-07-29 19:16:14 -0300

    LabelWrapper to Select component

--- components/Select/index.tsx
+++ components/Select/index.tsx
@@ -1,20 +1,24 @@
 import React from "react"
-import {SelectContainer, SelectInput } from "./styles"
+import {LabelWrapper, SelectContainer, SelectInput } from "./styles"
 
 interface SelectProps {
     data: {value: string, label: string}[]
     placeholder?: string
     w: string
+    register: any
+    errors: any
 }
 
-const Select = ({data, placeholder, w}: SelectProps) => {
+const Select = ({data, placeholder, w, register, errors}: SelectProps) => {
     return(
         <SelectContainer w={w}>
-            <label htmlFor="">
-                <p>{placeholder}</p>
-            </label>
-            <SelectInput>
-                <option hidden>{'Selecione'}</option>
+            <LabelWrapper>
+                <label htmlFor={register.name}>
+                    <p>{placeholder}</p>
+                </label>
+                <p>{errors && errors[register.name]?.message }</p>
+            </LabelWrapper>
+            <SelectInput {...register}>
             {data.map((option) => (
                 <option key={option.label} value={option.value}>{option.label}</option>
             ))}
--- components/Select/styles.ts
+++ components/Select/styles.ts
@@ -9,6 +9,17 @@ export const SelectContainer = styled.div<SelectContainerProps>`
     padding: 0.5rem;
 `
 
+export const LabelWrapper = styled.div`
+    display:  flex;
+    gap: 0.5rem;
+    align-items: center;
+
+    & > p {
+        color: ${props => props.theme['red-500']};
+        font-size: 0.75rem;
+    }
+`
+
 export const SelectInput = styled.select`
     width: 100%;
     height: 2.5rem;
