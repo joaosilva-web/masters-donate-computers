bc5260386f88334dbdc95a3018b9875466a462be
João Gustavo <joaogustavosilva2001@gmail.com>
2022-07-29 19:17:07 -0300

    Validate and Submit form

--- components/DataSubmissionForm/index.tsx
+++ components/DataSubmissionForm/index.tsx
@@ -1,20 +1,73 @@
-import { useEffect, useState } from 'react';
+import { FormEventHandler, useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form'
+import {zodResolver} from '@hookform/resolvers/zod'
 import Input from '../Input';
-import { FormDataContainer, FormGroupContainer } from './styles';
+import { Button, FormDataContainer, FormGroupContainer } from './styles';
+import * as zod from 'zod'
 
 import cep from 'cep-promise'
 import Select from '../Select';
 import Spinner from '../Spinner';
+import api from '../../services/api';
+
+const newDonationFormValidationSchema = zod.object({
+    name: zod.string().min(1, '*Informe o seu nome'),
+    email: zod.string().min(1, '*Informe o seu E-mail').email('Utilize um E-mail valido!'),
+    phone: zod.string().min(1, '*Informe o seu Telefone'),
+
+    zip: zod.string().min(1, '*Informe o seu CEP'),
+    city: zod.string().min(1, '*Informe sua cidade'),
+    state: zod.string().min(1, '*Informe seu estado'),
+    streetAddress: zod.string().min(1, '*Informe o seu endereço'),
+    number: zod.string().min(1, '*Informe o seu número'),
+    complement: zod.string().nullable(),
+    neighborhood: zod.string().min(1, '*Informe o seu bairro'),
+    deviceCount: zod.number().min(1, '*Informe a quantidade de dispositivos'),
+
+    type0: zod.string().min(1, '*Selecione o tipo'),
+    condition0: zod.string().min(1, '*Selecione a condição')
+})
 
 const DataSubmissionForm = () => {
-    const { register, handleSubmit, watch, setValue, setFocus, setError, trigger  } = useForm()
+    const { register, handleSubmit, watch, setValue, setFocus, formState:{errors}, getValues  } = useForm({
+        resolver: zodResolver(newDonationFormValidationSchema)
+    })
     const [loading, setLoading] = useState(false)
 
-    const zipcode = watch('zipcode')
-    const city = watch('city')
+    const zipcode = watch('zip')
     const deviceCount = watch('deviceCount')
+    
+    function handleNewDonation() {
+        const devices = [];
+        const fieldsValues = getValues()
+        for(let index = 0; index < deviceCount; index++) {
+            let type = fieldsValues[`type${index}`]
+            let condition = fieldsValues[`condition${index}`]
+            devices.push({ type, condition })
+        }
 
+        const dataToStore = {
+        name: fieldsValues.name,
+        email: fieldsValues.email,
+        phone: fieldsValues.phone,
+        zip: fieldsValues.zip,
+        city: fieldsValues.city,
+        state: fieldsValues.state,
+        streetAddress: fieldsValues.streetAddress,
+        number: fieldsValues.number,
+        complement: fieldsValues.complement,
+        neighborhood: fieldsValues.neighborhood,
+        deviceCount: fieldsValues.deviceCount,
+        devices
+        }
+        console.log(dataToStore)
+        api.post('donation', dataToStore).then((response) => {
+            console.log(response, 'RESPOSTA');
+        }).catch((error) => {
+            console.log(error, 'ERRO AO ENVIAR')
+        })
+    }
+    // FUNCTION THAT GET ADDRESS DATA
     async function getAddress(zipcode: string){
 
         const zipcodeFormatted = zipcode.replace(/\D/g, '')
@@ -32,12 +85,11 @@ const DataSubmissionForm = () => {
         }
 
     }
-    //GET ADDRESS DATA IF ZIPCODE INPUT HAS LENGTH EQUAL TO 9
-
+    
+    // USEEFECT TO GET ADDRESS DATA IF ZIPCODE INPUT HAS LENGTH EQUAL TO 9
     useEffect(() => {
         if(zipcode){
             if(zipcode.length >= 9){
-
                     setLoading(true)
                getAddress(zipcode).then((response) => {
                 console.log(response)
@@ -51,6 +103,7 @@ const DataSubmissionForm = () => {
             }
         }
     },[zipcode])
+
 
     // OPTIONS FOR DEVICE SELECT
     const deviceTypesData = [
@@ -67,38 +120,47 @@ const DataSubmissionForm = () => {
         {value: 'notWorking', label: 'Tem todas as partes, mas não liga mais'},
         {value: 'broken', label: 'Faltam peças, funciona só as vezes ou está quebrado'},
     ]
+
+    //FOR TO SHOW DEVICE INFORMATION
+    const deviceInformation = []
+    for(let index = 0; index < deviceCount; index++) {
+        deviceInformation.push(
+            <FormGroupContainer>
+                <Select data={deviceTypesData} w={'100%'} placeholder='Tipo do dispositivo' register={register(`type${index}`)} errors={errors}/>
+                <Select data={deviceStatesData} w={'100%'} placeholder='Estado do dispositivo' register={register(`condition${index}`)} errors={errors}/>
+            </FormGroupContainer>)
+    }
+
     return(
-        <FormDataContainer>
+        <FormDataContainer onSubmit={handleSubmit(handleNewDonation)} action="">
                 {loading && <Spinner/>}
              {/* PERSONAL DATA INPUTS */}
             <FormGroupContainer>
-            <Input inputType='text' w={'100%'} label='Nome' placeholder='João' register={register('name')}/>
-            <Input inputType='email' w={'50%'} label='E-mail' placeholder='joaosilva@gmail.com' register={register('email')}/>
-            <Input inputType='text' w={'50%'} label='Telefone' placeholder='(44) 99999-9999' mask='phone' register={register('phone')}/>
+            <Input inputType='text' w={'100%'} label='Nome' placeholder='João' register={register('name')} errors={errors}/>
+            <Input inputType='email' w={'50%'} label='E-mail' placeholder='joaosilva@gmail.com' register={register('email')} errors={errors}/>
+            <Input inputType='text' w={'50%'} label='Telefone' placeholder='(44) 99999-9999' mask='phone' register={register('phone')} errors={errors}/>
             </FormGroupContainer>
 
             {/* ADDRESS DATA INPUTS */}
             <FormGroupContainer>
-            <Input inputType='text' w={'50%'} label='CEP' placeholder='99999-999' mask='zipcode' register={register('zipcode')}/>
-            <Input inputType='text' w={'50%'} label='Cidade' placeholder='Toledo' mask='city' register={register('city')}/>
-            <Input inputType='text' w={'50%'} label='Estado' placeholder='PR' mask='state' register={register('state')}/>
-            <Input inputType='text' w={'50%'} label='Bairro' placeholder='Vila Becker' register={register('neighborhood')}/>
-            <Input inputType='text' w={'50%'} label='Endereço' placeholder='Av. São Paulo' register={register('streetAdress')}/>
-            <Input inputType='text' w={'50%'} label='Número' placeholder='Av. São Paulo' register={register('number')}/>
+            <Input inputType='text' w={'50%'} label='CEP' placeholder='99999-999' mask='zipcode' register={register('zip')} errors={errors}/>
+            <Input inputType='text' w={'50%'} label='Cidade' placeholder='Toledo' mask='city' register={register('city')} errors={errors}/>
+            <Input inputType='text' w={'50%'} label='Estado' placeholder='PR' mask='state' register={register('state')} errors={errors}/>
+            <Input inputType='text' w={'50%'} label='Bairro' placeholder='Vila Becker' register={register('neighborhood')} errors={errors}/>
+            <Input inputType='text' w={'50%'} label='Endereço' placeholder='Av. São Paulo' register={register('streetAddress')} errors={errors}/>
+            <Input inputType='text' w={'50%'} label='Complemento' placeholder='Ao lado do mercado São Lucas' register={register('complement')} errors={errors}/>
+            <Input inputType='text' w={'50%'} label='Número' placeholder='4878' register={register('number')} errors={errors}/>
             </FormGroupContainer>
 
             {/* AMOUNT OF DEDVICES TO DONATE */}
             <FormGroupContainer>
-            <Input inputType='number' w={'50%'} label='Quantos equipamentos serão doados' placeholder='2' register={register('deviceCount')}/>
+            <Input inputType='number' w={'50%'} label='Quantos equipamentos serão doados' placeholder='2' register={register('deviceCount', {valueAsNumber: true,  value: 1})} errors={errors}/>
             </FormGroupContainer>
 
             {/* DEVICES INFORMATIONS */}
-            <FormGroupContainer>
-            <Select data={deviceTypesData} w={'50%'} placeholder='Tipo do dispositivo'/>
-            <Select data={deviceStatesData} w={'50%'} placeholder='Estado do dispositivo'/>
-            </FormGroupContainer>
+            {deviceInformation}
 
-            <Input inputType='submit' w={'50%'} placeholder='Enviar'/>
+            <Button type='submit' w={'50%'}>Enviar</Button>
 
         </FormDataContainer>
     )
--- components/DataSubmissionForm/styles.ts
+++ components/DataSubmissionForm/styles.ts
@@ -16,3 +16,27 @@ export const FormGroupContainer = styled.div`
     width: 37.5rem; // 600px
     margin: 0 auto;
 `
+
+interface ButtonProps {
+    w: string
+}
+
+export const Button = styled.button<ButtonProps>`
+    margin: 0 auto;
+    width: 50%;
+    height: 2.5rem;
+    border: 0;
+    border-radius: 0.5rem;
+    padding: 0.5rem;
+    color: ${props => props.theme['gray-700']};
+    font-weight: bold;
+    background: ${props => props.theme['primary']};
+    color: ${props => props.theme['secondary']};
+
+    cursor: pointer;
+    transition: 0.5s ease;
+
+    &:hover {
+        background-color: ${props => props.theme['blue-500']};
+    }
+`
