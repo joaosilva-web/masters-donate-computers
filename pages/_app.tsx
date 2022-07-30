350c511708413c17ec18ddd0c1e2548bf9e4b0f3
João Gustavo <joaogustavosilva2001@gmail.com>
2022-07-30 10:33:40 -0300

    Add ToastContainer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
 import type { AppProps } from 'next/app'
+import { ToastContainer } from 'react-toastify'
 import { ThemeProvider } from 'styled-components'
 import { GlobalStyle } from '../styles/globals'
 import { defaultTheme } from '../styles/themes/default'
+import 'react-toastify/dist/ReactToastify.min.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -9,6 +11,16 @@ function MyApp({ Component, pageProps }: AppProps) {
       <ThemeProvider theme={defaultTheme}>
       <Component {...pageProps} />
       <GlobalStyle/>
+      <ToastContainer
+        className="impact-toast"
+        position="top-right"
+        autoClose={8000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        draggable={false}
+        closeOnClick
+        pauseOnHover
+      />
       </ThemeProvider>
     </>
   )
