dfe78325697be851b121342e799996e43b6bd9a7
João Gustavo <joaogustavosilva2001@gmail.com>
2022-07-30 17:58:44 -0300

    Create formGroupContainer component

---
+++ components/FormGroupContainer/index.tsx
@@ -0,0 +1,18 @@
+import { FormGroupWrapper } from "./styles"
+
+interface FormGroupContainerProps {
+    title: string
+    children: JSX.Element[] | JSX.Element
+}
+
+
+const FormGroupContainer = ({title, children}:FormGroupContainerProps) => {
+    return(
+        <FormGroupWrapper>
+            <p>{title}</p>
+            {children}
+        </FormGroupWrapper>
+    )
+}
+
+export default FormGroupContainer
---
+++ components/FormGroupContainer/styles.ts
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const FormGroupWrapper = styled.div`
+    position: relative;
+    display: flex;
+    flex-wrap: wrap;
+
+    border-top: 2px solid ${props => props.theme['gray-100']};
+    padding: 0.5rem 0;
+
+    width: 98%;
+    margin: 0 auto;
+
+    && > p {
+        background: ${props => props.theme['gray-50']};
+        color: ${props => props.theme['gray-300']};
+        position: absolute;
+        top: -14px;
+        left: 1rem;
+    }
+
+
+    @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    height: auto;
+  }
+
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+`
