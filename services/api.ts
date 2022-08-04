3ec380fd9f251127d88d8e50256b277c441595f1
João Gustavo <joaogustavosilva2001@gmail.com>
2022-08-04 06:29:22 -0300

    migrate API

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://doar-computador-api.herokuapp.com/",
+  baseURL: "https://secure-inlet-50293.herokuapp.com/",
 });
 
 export default api;
